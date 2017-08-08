aec182858f9c51b0b4a5c2947828205245476aab
jkxruby <jkxruby@gmail.com>
2017-08-08 20:19:10 +0800

    update temp

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -4,4 +4,12 @@ def index
   @cities = City.all
 end
 
+def update_temp
+  city = City.find(params[:id])
+  response = RestClient.get "http://v.juhe.cn/weather/index", :params => { :cityname => city.juhe_id, :key => "d945f85c8ee1761e206cf901c7f99d4d"}
+  data = JSON.parse(response.body)
+  city.update( :current_temp => data["result"]["sk"]["temp"])
+  redirect_to cities_path
+end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-resources :cities 
+resources :cities do
+  member do
+    post :update_temp
+  end
+end 
 
 end
